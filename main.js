8b09d474f6e2963006b4fa9cef2b964c1ff19510
Lauren Metz <Laurenmetz15@gmail.com>
2022-07-27 11:43:14 -0400

    commit tree 570816f2052f509420db2b1f87efdc27c6fcab67

--- main.js
+++ main.js
@@ -4,6 +4,111 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let errorMod = document.querySelector("#modal");
+errorMod.classList.add("hidden");
+let likes = document.querySelector(".like");
+console.log(likes);
+
+const liker = document.querySelectorAll('.like-glyph');
+liker.forEach(like => {
+  console.log(like);
+  like.addEventListener('click', function(event) {
+    if(like.classList.contains('activated-heart')) {
+      like.textContent = EMPTY_HEART;
+      like.classList.remove('activated-heart')
+    }
+    else {
+      mimicServerCall().then(function(resp) {
+        console.log(resp);
+        like.textContent = FULL_HEART;
+        like.classList.add('activated-heart');
+      }).catch(function(error) {
+        console.log(error);
+        errorMod.classList.remove("hidden");
+        errorMod.textContent = error.mesage;
+        window.setTimeout(errorMod.classList.add("hidden"),3000);
+      })
+      //like.textContent = FULL_HEART;
+      //like.classList.add('activated-heart');
+    }
+    
+  })
+})
+
+//let liker = document.getElementsByClassName("like-glyph");
+//liker is now an HTML collection of all the hearts 
+/*
+console.log(liker);
+//liker.forEach(handleLike);
+for(i = 0; i < liker.length; i++) {
+  console.log(liker.length);
+  liker[i].addEventListener('click', function(event) {
+    console.log(liker[i]);
+  })
+}
+*/
+/*    if(liker[i].classList.contains('activated-heart')) {
+      console.log('full heart');
+    }
+    else {
+      console.log('empty heart');
+    }
+    mimicServerCall().then((resp) => {
+      console.log('I was clicked');
+      console.log(i);
+    })
+    .catch(function(error) {
+      errorMod.classList.remove("hidden");
+      errorMod.textContent = error.mesage;
+      window.setTimeout(errorMod.classList.add("hidden"),3000);
+    });
+  });
+}
+*/
+/*
+for(like of liker) {
+  console.log(like);
+  console.log(like.textContent);
+  like.addEventListener('click', function(event) {
+    mimicServerCall().then(() => {
+      if(like.classList.contains('activated-heart')) {
+        like.textContent = EMPTY_HEART;
+        like.classList.remove('activated-heart')
+      }
+      else {
+        like.textContent = FULL_HEART;
+        like.classList.add('activated-heart');
+      }
+    })
+    .catch(function(error) {
+      errorMod.classList.remove("hidden");
+      errorMod.textContent = error.mesage;
+      window.setTimeout(errorMod.classList.add("hidden"),3000);
+    })
+  })
+}
+*/
+/*
+  function handleLike(like) {
+    like.addEventListener('click', function(event) {
+      mimicServerCall().then(() => {
+        if(liker.classList.contains('activated-heart')) {
+          liker.textContent = EMPTY_HEART;
+          liker.classList.remove('activated-heart')
+        }
+        else {
+          liker.textContent = FULL_HEART;
+          liker.classList.add('activated-heart');
+        }
+      })
+      .catch(function(error) {
+        errorMod.classList.remove("hidden");
+        errorMod.textContent = error.mesage;
+        window.setTimeout(errorMod.classList.add("hidden"),3000);
+      })
+    })
+  }
+  */
 
 
 
